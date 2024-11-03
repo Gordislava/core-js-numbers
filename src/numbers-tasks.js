d47ee64830a167ba00dce95e2375cd199dfc6537
Yaroslava Ponomareva <ponomareva@friendsonly.me>
2024-11-03 16:10:36 +0300

    feat: done 10 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -68,8 +68,11 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const axisX = (x2 - x1) ** 2;
+  const axisY = (y2 - y1) ** 2;
+
+  return Math.sqrt(axisX + axisY);
 }
 
 /**
@@ -84,8 +87,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
@@ -105,8 +108,15 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const multiplyX = x1 * x2;
+  const multiplyY = y1 * y2;
+  const sum = multiplyX + multiplyY;
+  const vector1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const vector2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cos = sum / (vector1 * vector2);
+
+  return Math.acos(cos);
 }
 
 /**
@@ -122,8 +132,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -137,8 +147,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -154,8 +164,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const powSum = a ** 2 + b ** 2 + c ** 2;
+  return Math.sqrt(powSum);
 }
 
 /**
